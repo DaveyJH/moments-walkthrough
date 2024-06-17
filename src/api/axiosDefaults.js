3dcadecc0673964de1d2dfbba449e311c2b02cdb
DaveyJH <daveyj.dh@googlemail.com>
2024-06-17 16:18:19 +0100

    feat: Add basic sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://react-walkthrough-42-a1578d503c9f.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
