42fd652647b59988e9e089b5110bc9a5b91339c9
DaveyJH <daveyj.dh@googlemail.com>
2024-06-25 22:12:26 +0100

    feat: Add postCreate front end and toggle navbar

---
+++ src/components/Asset.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import styles from "../assets/css/Asset.module.css";
+
+const Asset = ({ spinner, src, message, alt }) => {
+  return (
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && <Spinner animation="border" />}
+      {src && <img src={src} alt={alt || message} />}
+      {message && <p className="mt-4">{message}</p>}
+    </div>
+  );
+};
+
+export default Asset;
