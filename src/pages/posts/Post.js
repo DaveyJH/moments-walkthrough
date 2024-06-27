b00c5c21343b15917ccffba40189781904494710
DaveyJH <daveyj.dh@googlemail.com>
2024-06-27 19:53:02 +0100

    feat: Add post content and css

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "../../assets/css/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Avatar } from "../../components/Avatar";
+
+const Post = (props) => {
+  const {
+    id,
+    owner,
+    created_at,
+    updated_at,
+    title,
+    content,
+    image,
+    profile_id,
+    profile_image,
+    image_filter,
+    like_id,
+    comments_count,
+    likes_count,
+    postPage,
+  } = props;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Post;
